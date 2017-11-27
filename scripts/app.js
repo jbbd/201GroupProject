2b479e1d72849566221afabea761499fdb0697ae
Jules Duro <ourshieldoath@gmail.com>
2017-11-27 13:52:11 -0800

    ready for merge

--- scripts/app.js
+++ scripts/app.js
@@ -3,19 +3,25 @@
 var restroomList = []; //stores all restroom instances
 var userRestrooms = []; //list of restrooms based on user specs
 
-function restroom(name, address, clean, size, accessibility, orientation, changingStation, dispensers, toiletPaper, code){
+function restroom(name, address, clean, singleS, multiS, size, accessibility,
+  genderN, changingStation, dispensers, toiletPaper){
   this.name = name;
   this.address = address;
-  this.clean = clean; //bool
+  this.clean = clean;
+  this.singleStall = singleS; //bool
+  this.multiStall = multiS; //bool
   this.size = size;
-  this.hAccess = accessibility; //handicap accessible / bool
-  this.orientation = orientation; //array?
+  this.hAccess = accessibility; //bool
+  this.genderNeutral = genderN; //array?
   this.changingStation = changingStation; //bool
   this.dispensers = dispensers; //bool
   this.toiletPaper = toiletPaper;//bool
-  this.code = []; //holds bool, code (if applicable).
-  // this.location = location; //Stretch goal? Used for map data.
   restroomList.push(this);
+}
+
+function user(username, reviews){
+  this.username = username;
+  this.numOfReviews = reviews;
 }
 
 var userSpecs = {//for form input
@@ -32,13 +38,17 @@ var userSpecs = {//for form input
     //displays list to page
   },
 };
-//Does this belong in method?
-// var onClick = function(){
-//   //get click event
+
+// function saveData (){
+//   //store data into storage
+// }
+// function clearData(){
+//   //clears data from storage
 // };
-var storeData = function (){
-  //store data into storage
-};
-var clearData = function (){
-  //clears data from storage
-};
+
+// function createSectionEl(){
+//   //creates Section element for bathroom list
+// }
+// function createPhotoEl(){
+//   //creates a photo element to append to section
+// }
