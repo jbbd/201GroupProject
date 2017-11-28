aeffe52f9402c90e92ce813b6061a312ec23a14f
Yohanes Demissie <Yohanesdemissie@gmail.com>
2017-11-28 12:25:45 -0800

    GOT SALMON COOKIES INTO FROM.JS START POINT FOR COMMENT FORM

--- form.js
+++ form.js
@@ -75,7 +75,7 @@ function test(){//testing
 var stores = [];
 
 // CANNOT DELETE VAR HOURS
-var hours = [''];
+var hours = ['6:00 AM', '7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM','11:00 AM','12:00 PM','1:00 PM','2:00 PM','3:00 PM','4:00 PM','5:00 PM','6:00 PM','7:00 PM','8:00 PM','Total'];
 
 function Store (id, min, max, avg){
   this.id = id;
