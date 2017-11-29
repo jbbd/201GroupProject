e4507c4e10806c7f73bbe171c9807e11ebb3aeba
Sarah Bixler <bixler.s.e@gmail.com>
2017-11-29 15:00:56 -0800

    not sure here, think it's all working

--- print-both.js
+++ print-both.js
@@ -1,7 +1,7 @@
 'use strict';
 //Feel free to modify
 var restroomList = []; //stores all restroom instances
-//var userRestrooms = []; //list of restrooms based on user specs
+var restroomItems = ['Address: ', 'Cleanliness Rating: ', 'Is it Single Stall?  ', 'Is it Multi Stall', 'How big is it?  ', 'Is it Handicap Accessible']
 var restroomReview = [];
 var loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ';
 
