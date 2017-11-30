c4a69c5fddbd95b2b6aedd92493b0dd55752ba71
Jordan <JordanLesaca@Gmail.Com>
2017-11-30 14:34:44 -0800

    delete this

--- scripts/user-form.js
+++ scripts/user-form.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//var newUser = [];
+var newUserArray = [];
 var getForm = document.getElementById('main-form');
 // function User(username, review, location, rating){
 //   this.username = username;
@@ -14,15 +14,29 @@ var getForm = document.getElementById('main-form');
 function onSubmit(event) {
   event.preventDefault();
   console.log('form filled');
-
-  var userName = event.target.userName.value;
-  var userComment = event.target.userComment.value;
-  var userLocation = event.target.userLocation.value;
-  var userRating = event.target.userRating.value;
-  var newUser = [userName, userComment, userLocation, userRating];
-
-  localStorage.setItem('userdata', JSON.stringify(newUser));
+  var userForm = {
+    userName: event.taret.userName.value,
+    userComment: event.target.userComment.value,
+    userLocation: event.target.userLocation.value,
+    userRating: event.target.userRating.value,
+  };
+  var userFormData = [userForm.userName, userForm.userComment, userForm.userLocation, userForm.userRating];
+  newUserArray.push(userFormData);
+  console.log('newUserArray', newUserArray);
+  localStorage.setItem('userdata', JSON.stringify(newUserArray));
 }
+
+// var userName = event.target.userName.value;
+// var userComment = event.target.userComment.value;
+// var userLocation = event.target.userLocation.value;
+// var userRating = event.target.userRating.value;
+// var newUser = [userName, userComment, userLocation, userRating];
+// newUserArray.push(newUser);
+
+// localStorage.setItem('userdata', JSON.stringify(newUserArray));
+
+
+
 
 
 getForm.addEventListener('submit', onSubmit);
