e1ee9390d7f3ce24f5a019b42f40c3ab1c93c9ab
Yohanes Demissie <Yohanesdemissie@gmail.com>
2017-11-30 11:23:49 -0800

    Jordan already merged these files with welcome.html

--- login.js
+++ login.js
@@ -1,3 +1,32 @@
 'use strict';
 
-loginNames = []
+(function () {
+var password= document.getElementByld('password'); // Store password inputs
+var passwordConfirm = document.getElementByid('conf-password ' );
+function setErrorHighlighter(e) {
+// Get target element
+//If its length is< 8
+//Set class to fail
+// Otherwise
+// Set class to pass
+// Get target element
+// If class is fail
+// Clear class
+function passwordsMatch(e) {
+var target= e.target || e.srcElement;
+// If value matches pwd and it is longer than 8 characters
+if ((password.value=== target.value) && target.value.length>= 8){
+var target= e.target || e.srcElement; if (target.value.length< 8) {
+target.className ='fail';
+} else {
+target.className = 'pass';
+}
+
+addEvent(password, 'focus', removeErrorHighlighter);
+addEvent(password, 'blur', setErrorHighlighter);
+addEvent (passwordConfirm, 'focus' , removeErrorHigh1ighter); addEvent(passwordConfirm, ' b l u r ' , passwordsMatch);
+}());
+
+location.href
+var userBank = ['Sarah_B', 'Jordan_L', 'Wyatt_P', 'Jules_D', 'Yohanes_D'];
+var passwordBank = ['Password'];
